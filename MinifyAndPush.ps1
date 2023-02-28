c300a604b2c41924f63c03a488873f5d0f208dda
Sarah <justsweetluna@gmail.com>
2023-02-28 18:56:08 +0100

    remove duplicates

--- MinifyAndPush.ps1
+++ MinifyAndPush.ps1
@@ -1,5 +1,5 @@
 Write-Host "Minifying HTML files..."
-html-minifier -c "C:\Users\Sarah\AppData\Roaming\npm\node_modules\html-minifier\cli-config-file.conf" --file-ext html  --input-dir "./" --output-dir "./"
+html-minifier -c "./html-minifer-conf.conf" --file-ext html  --input-dir "./" --output-dir "./"
 Write-Host "Copying .htaccess files..."                                   
 gci ".\" -Dir -Rec | cpi ".\.htaccess" -D {$_.fullname}
 Write-Host "Git add, commit, and push..."                                       
